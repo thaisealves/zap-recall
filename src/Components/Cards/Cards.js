ae30f46b0aafe185376c152f263caaa870ec3a63
thaisealves <alves.thaisesilva@gmail.com>
2022-05-16 09:43:03 -0300

    Trying to open the question card

--- src/Components/Cards/Cards.js
+++ src/Components/Cards/Cards.js
@@ -1,44 +1,54 @@
 import logo from "../../assets/img/zapLogo.png"
+import turn from "../../assets/img/Vector.svg"
 import "./Cards.css"
 import React from "react"
+
+/* data from the cards */
+const cards = [
+    { question: "O que é JSX?", answer: " Uma extensão de linguagem do JavaScript" },
+    { question: " O React é __ ", answer: "uma biblioteca JavaScript para construção de interfaces" },
+    { question: "Componentes devem iniciar com __ ", answer: "letra maiúscula" },
+    { question: "Podemos colocar __ dentro do JSX", answer: "expressões" },
+    { question: "O ReactDOM nos ajuda __", answer: "interagindo com a DOM para colocar componentes React na mesma" },
+    { question: "Usamos o npm para __", answer: "gerenciar os pacotes necessários e suas dependências" },
+    { question: "Usamos props para __", answer: "passar diferentes informações para componentes " },
+    { question: "Usamos estado (state) para __", answer: "dizer para o React quais informações quando atualizadas devem renderizar a tela novamente" }
+]
+cards.sort(() => Math.random() - 0.5); /* getting the cards sorted */
 
 export default function Cards() {
     const QUESTIONS_NUM = 8; /* number of card on a deck */
     let enumQuestions = []; /* enumering the cards, first page from questions */
-    let questionCard; /* set the content of the card depending on status */
     const [complete, SetComplete] = React.useState(0) /* seeing how many cards where answered */
-    const [openCard, setOpenCard] = React.useState("closed") /* seeing if the card is closed or opened */
+    // const [selectedCard, setSelectedCard] = React.useState("");  /* the card that we clicked */
 
-    function CardHandle() {
-        setOpenCard("opened")
+    for (let i = 0; i < QUESTIONS_NUM; i++) {
+        enumQuestions.push(`Pergunta ${i + 1}`);
     }
+    // { enumQuestions.map((element, index) => <ClosedBtn key={index} element={element} index={index} />) }
 
-    function QuestionBtn({ element }) {
-        return (
-            <div className="questionBtn btn">
+
+    function ClosedBtn({element, index}) {
+        const [isCardOpen, setIsCardOpen] = React.useState("closed") /* seeing if the card is closed or opened */
+        let questionCard;  /* set the content of the card depending on status */
+        isCardOpen === "closed" ? /* what's the status of the card? */
+            questionCard =
+            (<div key={index} className="questionBtn btn">
                 {element}
                 <ion-icon name="play-outline" onClick={CardHandle}></ion-icon>
-            </div>
-        )
-    }
-    for (let i = 0; i < QUESTIONS_NUM; i++) {
-        enumQuestions.push(`Pergunta ${i + 1}`)
-    }
+            </div>) :
+            questionCard = (cards.map((elem, index) => <OpenedCard question={elem.question} answer={elem.answer} key={index} index={index} />));
 
-    const cards = [
-        { question: "O que é JSX?", answer: " Uma extensão de linguagem do JavaScript" },
-        { question: " O React é __ ", answer: "uma biblioteca JavaScript para construção de interfaces" },
-        { question: "Componentes devem iniciar com __ ", answer: "letra maiúscula" },
-        { question: "Podemos colocar __ dentro do JSX", answer: "expressões" },
-        { question: "O ReactDOM nos ajuda __", answer: "interagindo com a DOM para colocar componentes React na mesma" },
-        { question: "Usamos o npm para __", answer: "gerenciar os pacotes necessários e suas dependências" },
-        { question: "Usamos props para __", answer: "passar diferentes informações para componentes " },
-        { question: "Usamos estado (state) para __", answer: "dizer para o React quais informações quando atualizadas devem renderizar a tela novamente" }
-    ]
-    cards.sort(() => Math.random() - 0.5);
-    openCard === "closed" ?
-        questionCard = enumQuestions.map((element, index) => <QuestionBtn key={index} element={element} />) :
-        questionCard = cards.map((elem, index) => <OpenCard question={elem.question} answer={elem.answer} key={index} />)
+        return (
+            <>
+                {questionCard}
+            </>
+        )
+
+        function CardHandle(index) {
+            setIsCardOpen("opened");
+        }
+    }
 
     return (
         <div className="questions">
@@ -47,7 +57,7 @@ export default function Cards() {
                 <h1>ZapRecall</h1>
             </div>
             <div className="question">
-                {questionCard}
+            { enumQuestions.map((element, index) => <ClosedBtn key={index} element={element} index={index} />) }
             </div>
             <footer>
                 {complete}/{QUESTIONS_NUM} CONCLUÍDOS!
@@ -55,13 +65,34 @@ export default function Cards() {
         </div>
     )
 
-
-
 }
-function OpenCard({ question, answer }) {
-    return (
-        <div className="card-question opened-card">
+
+
+function OpenedCard({ question, answer }) {
+    const [cardContent, setCardContent] = React.useState("question")
+    let content;
+    cardContent === "question" ?
+        content =
+        <div className="card-question opened-card btn">
             {question}
+            <img className="turning" src={turn} alt="turning card" onClick={turningHandle} />
+        </div> :
+        content =
+        <div className="card-answer opened-card btn">
+            {answer}
+            <div className="buttons">
+                <button className="incorrect">Não lembrei</button>
+                <button className="almost">Quase não lembrei</button>
+                <button className="correct">Zap!</button>
+            </div>
         </div>
+    return (
+        <>
+            {content}
+        </>
+
     )
+    function turningHandle() {
+        setCardContent("answer")
+    }
 }
